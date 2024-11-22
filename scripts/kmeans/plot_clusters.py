4d34c740da3d38d40f27a9874a42720f347be595
Daniel <dn23601@essex.ac.uk>
2024-11-21 20:45:31 -0600

    Remove discrete color sequence from K-means and PCA scatter plots for improved clarity

--- scripts/kmeans/plot_clusters.py
+++ scripts/kmeans/plot_clusters.py
@@ -6,7 +6,8 @@ storage_conn_str = sys.argv[1]
 container_name = sys.argv[2]
 
 df = download_from_blob('clusters_kmeans', storage_conn_str, container_name, folder='parquets')
-fig = px.scatter(df, y='ID_Reserva', color='Cluster', title='Reservations by Date and K-means Cluster', color_discrete_sequence=px.colors.qualitative.Set1)
+df['Cluster'] = df['Cluster'].astype(str)
+fig = px.scatter(df, y='ID_Reserva', color='Cluster', title='Reservations by Date and K-means Cluster')
 fig.update_layout(
     xaxis_title='Date',
     yaxis_title='Reservation ID'
