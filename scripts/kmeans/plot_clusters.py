b1c10c0a8402f0124d205a2b4d89962b4f27ded7
Daniel <dn23601@essex.ac.uk>
2024-11-21 20:21:55 -0600

    Enhance cluster visualization by adding a discrete color sequence to scatter plots in KMeans and PCA scripts

--- scripts/kmeans/plot_clusters.py
+++ scripts/kmeans/plot_clusters.py
@@ -6,7 +6,7 @@ storage_conn_str = sys.argv[1]
 container_name = sys.argv[2]
 
 df = download_from_blob('clusters_kmeans', storage_conn_str, container_name, folder='parquets')
-fig = px.scatter(df, y='ID_Reserva', color='Cluster', title='Reservations by Date and K means Cluster')
+fig = px.scatter(df, y='ID_Reserva', color='Cluster', title='Reservations by Date and K means Cluster', color_discrete_sequence=px.colors.qualitative.Set1)
 fig.update_layout(
     xaxis_title='Date',
     yaxis_title='Reservation ID',
